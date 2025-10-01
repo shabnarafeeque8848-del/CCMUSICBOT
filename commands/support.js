5b95c0855f5ab8e4ab7f16f0d51a1c3639f10e2b
shabnarafeeque8848-del <shabnarafeeque8848@gmail.com>
2025-10-01 20:13:45 +0530

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -9,10 +9,8 @@ module.exports = {
     options: [],
     run: async (client, interaction, lang) => {
         try {
-            const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-            const githubLink = "https://github.com/GlaceYT";
-            const replitLink = "https://replit.com/@GlaceYT";
-            const youtubeLink = "https://www.youtube.com/@GlaceYT";
+            const supportServerLink = "https://discord.gg/QvqGHH9eJ5"
+            const youtubeLink = "https://youtube.com/@trx_zoro9090?si=NRVSlguHY8LosfiE";
 
             const embed = new EmbedBuilder()
                 .setColor('#b300ff')
